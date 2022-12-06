cc771e289be6ae6fa922d54fc1971dd39ee36b71
CastilloDel <delcastillodelarosadaniel@gmail.com>
2022-12-06 10:40:54 +0100

    Day6 second part

--- day6/main.go
+++ day6/main.go
@@ -7,30 +7,49 @@ import (
 
 func main() {
 	fmt.Println("The result for the first part is:", part1())
+	fmt.Println("The result for the second part is:", part2())
 }
 
 func part1() int {
-	file, err := ioutil.ReadFile("day6/input")
+	content, err := ioutil.ReadFile("day6/input")
 	if err != nil {
 		panic("Couldn't read the input")
 	}
-	content := string(file)
 
-	marker := []byte{content[0], content[1], content[2], content[3]}
+	marker := content[0:4]
 	for i := 4; i < len(content); i++ {
 		if checkAllDifferent(marker) {
 			return i
 		}
 		marker = append(marker[1:], content[i])
 	}
-	panic("Couldn't find a marker")
+	panic("Couldn't find an start marker")
+}
+
+func part2() int {
+	content, err := ioutil.ReadFile("day6/input")
+	if err != nil {
+		panic("Couldn't read the input")
+	}
+
+	marker := content[0:14]
+	for i := 14; i < len(content); i++ {
+		if checkAllDifferent(marker) {
+			return i
+		}
+		marker = append(marker[1:], content[i])
+	}
+	panic("Couldn't find a message marker")
 }
 
 func checkAllDifferent(marker []byte) bool {
-	return marker[0] != marker[1] &&
-		marker[0] != marker[2] &&
-		marker[0] != marker[3] &&
-		marker[1] != marker[2] &&
-		marker[1] != marker[3] &&
-		marker[2] != marker[3]
+	appearances := make(map[byte]bool)
+	for _, value := range marker {
+		_, ok := appearances[value]
+		if ok {
+			return false
+		}
+		appearances[value] = true
+	}
+	return true
 }
