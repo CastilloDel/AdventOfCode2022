d91dd42dbfe3bfb7b0544c95097745f07fc1b214
CastilloDel <delcastillodelarosadaniel@gmail.com>
2023-02-07 13:36:15 +0100

    Day12 second part

--- day12/main.go
+++ day12/main.go
@@ -7,19 +7,56 @@ import (
 	"strings"
 )
 
+type ProblemData struct {
+	grid  Grid
+	start Position
+	end   Position
+}
+
 func main() {
 	content, err := ioutil.ReadFile("day12/input")
 	if err != nil {
 		panic("Couldn't read the input")
 	}
+	input := parseInput(string(content))
 
-	fmt.Println("The result for the first part is:", part1(string(content)))
+	fmt.Println("The result for the first part is:", part1(input))
+	fmt.Println("The result for the second part is:", part2(input))
 }
 
-func part1(content string) int {
-	input := parseInput(content)
-	result := findMinimumSteps(input.grid, input.start, input.end)
-	return result
+func parseInput(s string) ProblemData {
+	grid := [][]int{}
+	start := Position{}
+	end := Position{}
+	for y, row := range strings.Split(s, "\n") {
+		grid = append(grid, []int{})
+		for x, position := range strings.Split(row, "") {
+			if position == "S" {
+				start = Position{x, y}
+				position = "a"
+			} else if position == "E" {
+				end = Position{x, y}
+				position = "z"
+			}
+			grid[y] = append(grid[y], int(position[0])-int('a'))
+		}
+	}
+	return ProblemData{grid, start, end}
+}
+
+func part1(input ProblemData) int {
+	return findMinimumSteps(input.grid, input.start, input.end)
+}
+
+func part2(input ProblemData) int {
+	min := math.MaxInt
+	for _, start := range findPositionsWithHeight(input.grid, 0) {
+		minimumSteps := findMinimumSteps(input.grid, start, input.end)
+		if minimumSteps < min {
+			min = minimumSteps
+		}
+	}
+	return min
 }
 
 type Position struct {
@@ -28,12 +65,6 @@ type Position struct {
 }
 
 type Grid = [][]int
-
-type ProblemData struct {
-	grid  Grid
-	start Position
-	end   Position
-}
 
 type PossiblePath struct {
 	current        Position
@@ -97,22 +128,15 @@ func manhattanDistance(grid Grid, pos1, pos2 Position) int {
 	return int(math.Abs(float64(pos1.x-pos2.x)) + math.Abs(float64(pos1.y-pos2.y)))
 }
 
-func parseInput(s string) ProblemData {
-	grid := [][]int{}
-	start := Position{}
-	end := Position{}
-	for y, row := range strings.Split(s, "\n") {
-		grid = append(grid, []int{})
-		for x, position := range strings.Split(row, "") {
-			if position == "S" {
-				start = Position{x, y}
-				position = "a"
-			} else if position == "E" {
-				end = Position{x, y}
-				position = "z"
+func findPositionsWithHeight(grid Grid, desiredHeight int) []Position {
+	positions := []Position{}
+	for y, row := range grid {
+		for x, height := range row {
+			if height == desiredHeight {
+				positions = append(positions, Position{x, y})
 			}
-			grid[y] = append(grid[y], int(position[0])-int('a'))
+
 		}
 	}
-	return ProblemData{grid, start, end}
+	return positions
 }
